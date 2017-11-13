dc977598a3f63a0db485dded33e475e2f190c353
Boymoon <atdr7787@hotmail.com>
2017-11-13 21:17:34 +0300

    Update #1
    Adding a trial

---
+++ Instagram.App/Trial/Trial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ar_Project
+{
+    public class Trial
+    {
+        /// <summary>
+        /// اذا انتهت المرحلة التجريبية
+        /// </summary>
+        public bool IsTrialFinishes { get
+            {
+                return (timestorun == 0) ? true : false;
+            } }
+        /// <summary>
+        /// عدد مرات التشغيل
+        /// </summary>
+        public int timestorun { get { return GetTimestorun(); } set { } }
+        /// <summary>
+        /// عدد مرات التشغيل
+        /// </summary>
+        /// <returns>عدد مرات التشغيل المتبقية</returns>
+        private int GetTimestorun()
+        {
+            return Properties.Settings.Default.Timestorun;
+        }
+        /// <summary>
+        /// عند الدخول للبرنامج
+        /// </summary>
+        public void NewSession()
+        {
+            Properties.Settings.Default.Timestorun+= - 1;
+        }
+        
+    }
+}
